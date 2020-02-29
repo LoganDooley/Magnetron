a09355f6329e7ffe0c9baf740ae0639fc63bbcce
LoganDooley <logan_dooley@brown.edu>
2020-02-29 15:57:27 -0500

    Added magnets and movement functionality

---
+++ Assets/Scripts/MagneticPull.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticPull : MonoBehaviour
+{
+    public GameObject player;
+
+    public bool active = false;
+
+    public Vector2 standard;
+
+    public void Start()
+    {
+        standard = gameObject.transform.position;
+        gameObject.transform.position = Vector2.up * 1000;
+    }
+
+    public void toggle()
+    {
+        print("Q");
+        if (active)
+        {
+            active = false;
+            gameObject.transform.position = Vector2.up * 1000;
+            player.GetComponent<PlayerMvt>().pullSet(false);
+        }
+        else
+        {
+            active = true;
+            gameObject.transform.position = standard;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player" && active)
+        {
+            player.GetComponent<PlayerMvt>().pullSet(true);
+            print("collision");
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player" && active)
+        {
+            player.GetComponent<PlayerMvt>().pullSet(false);
+            print("exit");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMvt.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMvt : MonoBehaviour
+{
+    public float accel = 500f;
+    private Vector3 vel = Vector3.zero;
+    public float jump_accel = 0.1f;
+    public float max_speed = 1000f;
+    public bool can_jump = false;
+    public bool pull = false;
+    public float magnet_accel = 0.1f;
+
+    public void FixedUpdate()
+    {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        if(Mathf.Abs(rb.velocity.x) < max_speed)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(Vector2.left * accel);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(Vector2.right * accel);
+            }
+        }
+        if(Input.GetKey(KeyCode.W) && can_jump && Mathf.Abs(rb.velocity.y) < 0.001)
+        {
+            rb.AddForce(Vector2.up * jump_accel, ForceMode2D.Impulse);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.GetType() == typeof(BoxCollider2D))
+        {
+            can_jump = true;
+        }
+    }
+
+    public void pullSet(bool check)
+    {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        pull = check;
+        if (pull)
+        {
+            rb.gravityScale = 0;
+        }
+        else
+        {
+            rb.gravityScale = 3;
+        }
+    }
+}
