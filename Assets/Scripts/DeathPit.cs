228320c73d4e51a724ac1ab50a177b5a733f2764
LoganDooley <logan_dooley@brown.edu>
2020-03-07 14:59:52 -0500

    Added scrolling and death pits

---
+++ Assets/Scripts/DeathPit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DeathPit : MonoBehaviour
+{
+    public Text _killed;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            print("collision");
+            _killed.GetComponent<Text>().text = "Die";
+        }
+    }
+}
---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    public GameObject player;
+    private float _min = -3f;
+    private float _max = 3f;
+    private float _cameraVel = 0.1f;
+
+    void FixedUpdate()
+    {
+        if(player.transform.position.x-gameObject.transform.position.x < _min)
+        {
+            gameObject.transform.Translate(Vector3.left * _cameraVel);
+        }
+        if(player.transform.position.x - gameObject.transform.position.x > _max)
+        {
+            gameObject.transform.Translate(Vector3.right * _cameraVel);
+        }
+    }
+}
